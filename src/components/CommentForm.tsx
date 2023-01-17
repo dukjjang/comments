f4f2a0137f654888d7e5b1653e92655e700d6c74
dukjjang <jhd7292@naver.com>
2023-01-17 17:17:55 +0900

    design: input css 수정

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -2,13 +2,13 @@ import { INPUTS } from "../constants/index";
 
 const CommentForm = () => {
   return (
-    <form className="flex flex-col px-5 gap-2 bg-white">
+    <form className="flex flex-col p-5 gap-2 bg-white">
       {INPUTS.map((input) => {
         if (input.name === "content")
-          return <textarea className="input" {...input} />;
-        else return <input className="input" {...input} />;
+          return <textarea key={input.name} className="input" {...input} />;
+        else return <input key={input.name} className="input" {...input} />;
       })}
-      <button className="btn" type="submit">
+      <button className="btn w-14 text-sm" type="submit">
         등록
       </button>
     </form>
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,6 +1,6 @@
 export const INPUTS = [
-  { name: "profile", placeHolder: "https://picsum.photos/id/1/50/50" },
-  { name: "author", placeHolder: "작성자" },
-  { name: "content", placeHolder: "내용" },
-  { name: "createdAt", placeHolder: "2022-01-17" },
+  { name: "profile", placeholder: "https://picsum.photos/id/1/50/50" },
+  { name: "author", placeholder: "작성자" },
+  { name: "content", placeholder: "내용" },
+  { name: "createdAt", placeholder: "2022-01-17" },
 ];
